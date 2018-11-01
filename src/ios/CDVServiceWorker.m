7a74141bb3ea1ca2016bfdb3719303c440751c91
James N. V. Cash <james.nvc@gmail.com>
2018-10-31 21:46:36 -0400

    Set up registering service worker

--- src/ios/CDVServiceWorker.m
+++ src/ios/CDVServiceWorker.m
@@ -4,31 +4,61 @@
 
 - (void)pluginInitialize
 {
-  [self prepareJavascriptContext];
+    NSLog(@"Initing service worker plugin");
+    [self prepareJavascriptContext];
 }
 
 #pragma mark - "Service Worker" context
 
 - (void)prepareJavascriptContext
 {
-  self.jsContext = [[JSContext alloc] init];
+    self.jsContext = [[JSContext alloc] init];
+    self.jsContext.name = @"fake Service Worker";
+    NSString *shimScript = [NSString stringWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"service_worker_shim" withExtension:@"js"]
+                                                    encoding:NSUTF8StringEncoding
+                                                       error:nil];
+    [self.jsContext evaluateScript:shimScript];
 }
 
 #pragma mark - Methods called from javascript client
 
 - (void)register:(CDVInvokedUrlCommand*)command
 {
-  NSLog(@"Registering Service worker");
+    NSLog(@"Registering Service worker");
+    [self.commandDelegate runInBackground:^{
+        NSString *scriptUrl = [command argumentAtIndex:0];
+        NSDictionary *options = [command argumentAtIndex:1];
+        NSString *scope = options[@"scope"];
+
+        NSURL *srcUrl = [[NSBundle mainBundle] URLForResource:scriptUrl withExtension:nil subdirectory:@"www"];
+
+        NSLog(@"%@ scope %@ -> %@", scriptUrl, scope, srcUrl);
+
+        NSString *script = [NSString stringWithContentsOfURL:srcUrl
+                                                    encoding:NSUTF8StringEncoding
+                                                       error:nil];
+        [self.jsContext evaluateScript:script];
+
+        [self.commandDelegate
+         sendPluginResult:[CDVPluginResult
+                           resultWithStatus:CDVCommandStatus_OK
+                           messageAsDictionary:@{@"installing": [NSNull null],
+                                                 @"waiting": [NSNull null],
+                                                 @"active": @{@"scriptURL": scriptUrl},
+                                                 @"registeringScriptURL": scriptUrl,
+                                                 @"scope": scope}]
+         callbackId:command.callbackId];
+    }];
 }
 
 - (void)serviceWorkerReady:(CDVInvokedUrlCommand*)command
 {
-  NSLog(@"Service worker ready");
+    NSLog(@"Service worker ready");
 }
 
 - (void)postMessage:(CDVInvokedUrlCommand*)command
 {
-  NSLog(@"Posting message");
+    NSLog(@"Posting message");
 }
 
 @end
