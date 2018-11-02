6c8d8e5b5e3fa5e897711b981eb25a94390e8fc1
James N. V. Cash <james.nvc@gmail.com>
2018-11-02 12:26:42 -0400

    WIP

--- www/service_worker.js
+++ www/service_worker.js
@@ -3,6 +3,10 @@ const exec = require('cordova/exec');
 let ServiceWorker = function() { };
 
 ServiceWorker.prototype.postMessage = function(message, transfers) {
+  // TODO: Need to translate messageports somehow?
+  // or handle them here...
+  // make the postMessage thing return a value & we can put it into
+  // the port here.
   exec(null, null, "ServiceWorker", "postMessage", [message, transfers]);
 };
 
